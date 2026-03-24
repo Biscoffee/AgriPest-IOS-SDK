847699c6df1bec57f593bd37dbcbe99da25b80dc
sunwenyu <sunwenyu@bytedance.com>
2026-03-24 21:21:27 +0800

    chore: release version 1.0.40

---
+++ AgriPestClient/Model/AGPageAdminUserResponseDto.m
@@ -0,0 +1,34 @@
+#import "AGPageAdminUserResponseDto.h"
+
+@implementation AGPageAdminUserResponseDto
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"totalPages": @"totalPages", @"totalElements": @"totalElements", @"pageable": @"pageable", @"numberOfElements": @"numberOfElements", @"sort": @"sort", @"number": @"number", @"first": @"first", @"last": @"last", @"size": @"size", @"content": @"content", @"empty": @"empty" }];
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"totalPages", @"totalElements", @"pageable", @"numberOfElements", @"sort", @"number", @"first", @"last", @"size", @"content", @"empty"];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
